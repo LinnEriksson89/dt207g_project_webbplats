ffefcc617fe8bfdc1904e967ca2edf2fb3ed0409
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-09-06 17:18:45 +0200

    setting up gulp

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+/* DT207G - Backend-baserad webbutveckling
+ * Projekt
+ * Linn Eriksson, VT24
+ */
+
+//Variables to include in NPM-packades.
+const {src, dest, watch, series, parallel} = require("gulp");
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const ejs = require("gulp-ejs");
+
+//Paths
+const files = {
+    sassPath: "src/css/**/*.scss",
+    imagePath: "src/images/*",
+    ejsPath: "src/views/*.ejs"
+}
+
+//SASS-task
+function sassTask() {
+    return src(files.sassPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(dest("public/css"))
+    .pipe(browserSync.stream());
+}
+
+//Image-task
+function imageTask() {
+    return src(files.imagePath)
+    .pipe(dest('public/images'));
+}
+
+//ejs-task
+function ejsTask() {
+    return src(files.ejsPath)
+    .pipe(ejs())
+    .pipe(dest("views"))
+}
+
+//Watcher
+function watchTask() {
+
+    browserSync.init({
+        server: "/"
+    });
+
+    watch([files.sassPath, files.imagePath, files.ejsPath],
+    parallel(sassTask, imageTask, ejsTask))
+    .on('change', browserSync.reload);
+}
+
+//Run all tasks above
+exports.default = series(
+    parallel(sassTask, imageTask, ejsTask),
+    watchTask
+);
