cb06e40086099fcafc675010dbc33a31e7d62b28
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-09-08 17:52:28 +0200

    correction of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,15 @@ const {src, dest, watch, series, parallel} = require("gulp");
 const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify-es').default;
 
 //Paths
 const files = {
     sassPath: "src/css/**/*.scss",
     imagePath: "src/images/*",
     ejsPath: "src/views/*.ejs",
-    partialsPath: "src/views/partials/*ejs"
+    partialsPath: "src/views/partials/*ejs",
+    jsPath: "src/js/**/*.js"
 }
 
 //SASS-task
@@ -38,12 +40,20 @@ function ejsTask() {
     return src(files.ejsPath)
     .pipe(dest("views"))
 }
+
 //partials task
 function partialsTask() {
     return src(files.partialsPath)
     .pipe(dest("views/partials"))
 }
 
+
+//JS-task
+function jsTask() {
+    return src(files.jsPath)
+    .pipe(uglify())
+    .pipe(dest('public/js'));
+}
 
 //Watcher
 function watchTask() {
@@ -52,13 +62,13 @@ function watchTask() {
         server: "/"
     });
 
-    watch([files.sassPath, files.imagePath, files.ejsPath, files.partialsPath],
-    parallel(sassTask, imageTask, ejsTask, partialsTask))
+    watch([files.sassPath, files.imagePath, files.ejsPath, files.partialsPath, files.jsPath],
+    parallel(sassTask, imageTask, ejsTask, partialsTask, jsTask))
     .on('change', browserSync.reload);
 }
 
 //Run all tasks above
 exports.default = series(
-    parallel(sassTask, imageTask, ejsTask, partialsTask),
+    parallel(sassTask, imageTask, ejsTask, partialsTask, jsTask),
     watchTask
 );
